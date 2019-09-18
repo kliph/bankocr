11dfeaae4ad629fa87f60e1b00228dbe4c8cdb0f
Cliff Rodgers <kliph@users.noreply.github.com>
2019-09-18 07:58:04 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# bankocr
+
+A Clojure library designed to solve the [Bank OCR problem](http://codingdojo.org/kata/BankOCR/#resources).
+
+## Usage
+
+FIXME
---
+++ project.clj
@@ -0,0 +1,7 @@
+(defproject bankocr "0.1.0-SNAPSHOT"
+  :description "A Clojure library designed to solve http://codingdojo.org/kata/BankOCR/"
+  :url "https:/github.com/kliph/bankocr"
+  :license {:name "Apache-2.0"
+            :url "https://www.apache.org/licenses/LICENSE-2.0"}
+  :dependencies [[org.clojure/clojure "1.10.0"]]
+  :repl-options {:init-ns bankocr.core})

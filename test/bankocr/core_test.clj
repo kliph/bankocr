5e242366113464b4a17bdfebcac68ea1b48592cc
Cliff Rodgers <kliph@users.noreply.github.com>
2019-09-18 16:07:30 -0400

    Implement suggested test cases

--- test/bankocr/core_test.clj
+++ test/bankocr/core_test.clj
@@ -6,6 +6,33 @@
   (testing "parses an Account Number of all zeros"
     (is (= (first (ocr/parse-document "./resources/all-zeros.txt"))
            '(0 0 0 0 0 0 0 0 0))))
+  (testing "parses an Account number of all ones"
+    (is (= (first (ocr/parse-document "./resources/all-ones.txt"))
+           '(1 1 1 1 1 1 1 1 1))))
+  (testing "parses an Account number of all twos"
+    (is (= (first (ocr/parse-document "./resources/all-twos.txt"))
+           '(2 2 2 2 2 2 2 2 2))))
+  (testing "parses an Account number of all threes"
+    (is (= (first (ocr/parse-document "./resources/all-threes.txt"))
+           '(3 3 3 3 3 3 3 3 3))))
+  (testing "parses an Account number of all fours"
+    (is (= (first (ocr/parse-document "./resources/all-fours.txt"))
+           '(4 4 4 4 4 4 4 4 4))))
+  (testing "parses an Account number of all fives"
+    (is (= (first (ocr/parse-document "./resources/all-fives.txt"))
+           '(5 5 5 5 5 5 5 5 5))))
+  (testing "parses an Account number of all sixes"
+    (is (= (first (ocr/parse-document "./resources/all-sixes.txt"))
+           '(6 6 6 6 6 6 6 6 6))))
+  (testing "parses an Account number of all sevens"
+    (is (= (first (ocr/parse-document "./resources/all-sevens.txt"))
+           '(7 7 7 7 7 7 7 7 7))))
+  (testing "parses an Account number of all eights"
+    (is (= (first (ocr/parse-document "./resources/all-eights.txt"))
+           '(8 8 8 8 8 8 8 8 8))))
+  (testing "parses an Account number of all nines"
+    (is (= (first (ocr/parse-document "./resources/all-nines.txt"))
+           '(9 9 9 9 9 9 9 9 9))))
   (testing "parses a Document with multiple Account Number Entries"
     (is (= (ocr/parse-document "./resources/multiple-entries.txt")
            '((0 0 0 0 0 0 0 0 0)

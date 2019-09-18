c9cf8097ece5a8a410dde6088de35e4be7c26b00
Cliff Rodgers <kliph@users.noreply.github.com>
2019-09-18 11:51:44 -0400

    Confirm that parsing multiple account numbers works

--- test/bankocr/core_test.clj
+++ test/bankocr/core_test.clj
@@ -5,4 +5,8 @@
 (deftest parse-document
   (testing "parses an Account Number of all zeros"
     (is (= (first (ocr/parse-document "./resources/all-zeros.txt"))
-           '(0 0 0 0 0 0 0 0 0)))))
+           '(0 0 0 0 0 0 0 0 0))))
+  (testing "parses a Document with multiple Account Number Entries"
+    (is (= (ocr/parse-document "./resources/multiple-entries.txt")
+           '((0 0 0 0 0 0 0 0 0)
+             (0 0 0 0 0 0 0 0 0))))))
